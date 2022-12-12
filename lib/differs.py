44c2dba1048dbdf2abaed9657a7f67c79b00e58d
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-12-12 22:12:04 +0100

    feat: text differ

---
+++ lib/differs.py
@@ -0,0 +1,28 @@
+from difflib import SequenceMatcher
+
+
+def differ(model_transcript: str, yt_transcript: str):
+    results = {'replace': [], 'delete': [], 'insert': []}
+
+    model_set = model_transcript.split()
+    yt_set = yt_transcript.split()
+
+    seq_matcher = SequenceMatcher(a=model_set, b=yt_set)
+
+    for tag, alo, ahi, blo, bhi in seq_matcher.get_opcodes():
+        if tag == 'replace':
+            for a, b in zip(model_set[alo:ahi], yt_set[blo:bhi]):
+                results[tag].append((a, b))
+        elif tag == 'delete':
+            results[tag].extend(model_set[alo:ahi])
+        elif tag == 'insert':
+            results[tag].extend(yt_set[blo:bhi])
+
+    s = len(results['replace'])
+    i = len(results['delete'])
+    d = len(results['insert'])
+
+    results['WER'] = (s+i+d)/len(model_set)
+    results['matchRatio'] = seq_matcher.ratio()
+
+    return results
