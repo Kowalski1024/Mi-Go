d71a9a230f39a0a591fc92aaf7c274dbf05cce88
Gochad <malgorzata.drag@spoton.com>
2023-11-21 10:14:24 +0100

    remove unused test model

--- apptests/test_model.py
+++
@@ -1,7 +0,0 @@
-class TestModel:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def transcribe(audio, verbose=False, language="en"):
-        return {"text": "This is a model for tests :)", "language": "en"}
