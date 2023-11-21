da3ef38392ed6459e53d0c053d2733b9d5edd6c2
Gochad <malgorzata.drag@spoton.com>
2023-11-21 14:07:37 +0100

    fix format

--- apptests/test_generators.py
+++ apptests/test_generators.py
@@ -2,16 +2,17 @@ import unittest
 
 from generators.youtube_generator import videos_details_request
 
-empty_response = {'etag': 'YIUPVpqNjppyCWOZfL-19bLb7uk', 
-                      'items': [], 
-                      'kind': 'youtube#videoListResponse',
-                      'pageInfo': {'resultsPerPage': 0, 'totalResults': 0}
-                      }
+empty_response = {
+    "etag": "YIUPVpqNjppyCWOZfL-19bLb7uk",
+    "items": [],
+    "kind": "youtube#videoListResponse",
+    "pageInfo": {"resultsPerPage": 0, "totalResults": 0},
+}
 
 
 class Test_videos_details_request(unittest.TestCase):
     def test_check_returned_type(self):
-        self.assertEqual(type(videos_details_request([])), dict)
+        self.assertIsInstance(videos_details_request([]), dict)
 
     def test_check_empty_list(self):
         self.assertEqual(videos_details_request([]), empty_response)
@@ -19,5 +20,6 @@ class Test_videos_details_request(unittest.TestCase):
     def test_check_invalid_ids(self):
         self.assertEqual(videos_details_request(["invalid_id"]), empty_response)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
