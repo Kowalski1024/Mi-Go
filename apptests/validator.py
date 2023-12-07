bb36cd76eef06044c5e39f345c8e29ddf5e3fa3c
Gochad <malgorzata.drag@spoton.com>
2023-12-07 22:16:27 +0100

    draft validate

--- apptests/validator.py
+++ apptests/validator.py
@@ -2,32 +2,48 @@ import argparse
 import models
 
 from src.dataclasses import YouTubeVideo
+from youtube_transcript_api import YouTubeTranscriptApi
 
 class ModelClassValidator:
-    def __init__(self, class_name): 
+    def __init__(self, class_name, language, model_type): 
         module_attrs = vars(models)
         model_class = module_attrs.get(class_name)
-        self.model = model_class()
+        self.model_instance = model_class()
+        self.language = language
+        self.model_type = model_type
 
-        
-        
+    def get_yt_transcript(self, video_id: str) -> str:
+        transcripts = YouTubeTranscriptApi.list_transcripts(video_id)
+    
+        srt = transcripts.find_generated_transcript(
+            language_codes=["en"]
+        ).fetch()
+
+        return " ".join(fragment["text"] for fragment in srt)
+
 
     def validate(self):
-        returned_transcript = self.model.transcribe("apptests/sample.mp3")
-
-        # title: str
-        # videoId: str
-        # defaultAudioLanguage: str
-        # generatedTranscripts: list
-        # manuallyCreatedTranscripts: list
+        if not hasattr(self.model_instance, 'model'):
+            print("you must provide model")
+        if not hasattr(self.model_instance, 'transcriber'):
+            print("you must provide transcriber")
+        if not hasattr(self.model_instance, 'differ'):
+            print("you must provide differ")
+            
+        try:
+            returned_transcript = self.model_instance.transcribe("apptests/sample.mp3")
+            
+        except NotImplementedError as e:
+            print("you must provide transcribe method")
+            return
         
-        
-        video = YouTubeVideo.from_dict({"title": "gtaV", "videoId": "QkkoHAzjnUs", "defaultAudioLanguage": "en", "generatedTranscripts": ["en"], "manuallyCreatedTranscripts": ["en"]})
-          
-        target_transcript = video.youtube_transcript("en")
-
-        self.model_class.compare(returned_transcript, target_transcript)
-
+        target_transcript = self.get_yt_transcript("QkkoHAzjnUs")
+        try:
+            self.model_instance.compare(returned_transcript, target_transcript) 
+        except Exception as e:
+            print("you must provide compare method")
+            print(type(e).__name__)
+            return
 
 
 def command_parser() -> argparse.Namespace:
@@ -47,7 +63,7 @@ def command_parser() -> argparse.Namespace:
     )
     parser.add_argument(
         "-l",
-        "--relevanceLanguage",
+        "--relevance-language",
         required=False,
         type=str,
         default="en",
@@ -64,7 +80,7 @@ def command_parser() -> argparse.Namespace:
 
 def main():
     args = vars(command_parser())
-    mc = ModelClassValidator(args["class_name"])
+    mc = ModelClassValidator(args["class_name"], args["relevance_language"], args["model_type"])
     mc.validate()
 
 
