0dfc8a05a464a535db2712b4a24e38cc3b32c710
Gochad <malgorzata.drag@spoton.com>
2023-12-15 09:53:36 +0100

    add tests for insert func

--- apptests/test_yt_db.py
+++ apptests/test_yt_db.py
@@ -10,6 +10,7 @@ from src.database.youtube import (
     YouTubeTestPlan,
     YouTubeVideo,
 )
+from src.utils import insert_youtube_result
 
 
 class TestYouTubeModels(unittest.TestCase):
@@ -53,7 +54,7 @@ class TestYouTubeModels(unittest.TestCase):
         self.assertEqual(result.page_token, "token")
         self.assertEqual(result.q, "test")
         self.assertEqual(result.total_results, 1)
-        self.assertEqual(result.success, 0)  # No "results" in testplan_data["items"]
+        self.assertEqual(result.success, 0) 
         self.assertEqual(result.etag, "abc123")
 
     def test_youtube_video(self):
@@ -177,7 +178,7 @@ class TestYouTubeModels(unittest.TestCase):
             .first()
         )
         self.assertIsNotNone(result)
-        self.assertEqual(result.success, 0)  # No "results" in testplan_data["items"]
+        self.assertEqual(result.success, 0)
 
     def test_youtube_testplan_with_success(self):
         testplan_data = {
@@ -327,3 +328,213 @@ class TestYouTubeModels(unittest.TestCase):
         result = self.session.query(YouTubeResult).first()
         self.assertIsNotNone(result)
         self.assertEqual(result.model_settings, "{'param': 'value'}")
+
+
+class TestInsertYouTubeResult(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine("sqlite:///:memory:")
+        YouTubeTestPlan.metadata.create_all(engine)
+        YouTubeVideo.metadata.create_all(engine)
+        YouTubeResult.metadata.create_all(engine)
+        self.session = Session(engine)
+
+    def tearDown(self):
+        self.session.close()
+
+    def test_insert_youtube_result_success(self):
+        testplan_name = "test_plan"
+        testplan_data = {
+            "args": {
+                "maxResults": 10,
+                "pageToken": "token",
+                "q": "test",
+                "regionCode": "US",
+                "relevanceLanguage": "en",
+                "topicId": "1",
+                "videoCategoryId": "1",
+                "videoDuration": "1",
+                "videoLicense": "1",
+            },
+            "items": [
+                {
+                    "title": "1",
+                    "channelId": "1",
+                    "channelTitle": "1",
+                    "defaultAudioLanguage": "en",
+                    "videoId": "123",
+                    "results": {
+                        "modelName": "TestModel",
+                        "language": "en",
+                        "wer": 0.1,
+                        "mer": 1,
+                        "wil": 1,
+                        "wip": 1,
+                        "hits": 1,
+                        "substitutions": 12,
+                        "deletions": 12,
+                        "insertions": 65,
+                    },
+                    "duration": "12",
+                    "publishTime": "2022-01-01T00:00:00Z",
+                }
+            ],
+            "nextPageToken": "token",
+            "etag": "abc123",
+        }
+        success = insert_youtube_result(self.session, testplan_name, testplan_data)
+        self.assertTrue(success)
+
+        testplan = (
+            self.session.query(YouTubeTestPlan)
+            .filter_by(testplan_name=testplan_name)
+            .first()
+        )
+        self.assertIsNotNone(testplan)
+        self.assertEqual(testplan.total_results, 1)
+
+        video = self.session.query(YouTubeVideo).filter_by(video_id="123").first()
+        self.assertIsNotNone(video)
+
+        result = self.session.query(YouTubeResult).first()
+        self.assertIsNotNone(result)
+        self.assertEqual(result.model_name, "TestModel")
+        self.assertEqual(result.language, "en")
+        self.assertEqual(result.wer, 0.1)
+        self.assertEqual(result.mer, 1)
+        self.assertEqual(result.wil, 1)
+        self.assertEqual(result.wip, 1)
+
+    def test_insert_youtube_result_duplicate_testplan(self):
+        testplan_name = "test_plan"
+        testplan_data = {
+            "args": {
+                "maxResults": 10,
+                "pageToken": "token",
+                "q": "test",
+                "regionCode": "US",
+                "relevanceLanguage": "en",
+                "topicId": "1",
+                "videoCategoryId": "1",
+                "videoDuration": "1",
+                "videoLicense": "1",
+            },
+            "items": [
+                {
+                    "title": "1",
+                    "channelId": "1",
+                    "channelTitle": "1",
+                    "defaultAudioLanguage": "en",
+                    "videoId": "123",
+                    "results": {
+                        "modelName": "TestModel",
+                        "language": "en",
+                        "wer": 0.1,
+                        "mer": 1,
+                        "wil": 1,
+                        "wip": 1,
+                        "hits": 1,
+                        "substitutions": 12,
+                        "deletions": 12,
+                        "insertions": 65,
+                    },
+                    "duration": "12",
+                    "publishTime": "2022-01-01T00:00:00Z",
+                }
+            ],
+            "nextPageToken": "token",
+            "etag": "abc123",
+        }
+
+        insert_youtube_result(self.session, testplan_name, testplan_data)
+        success = insert_youtube_result(self.session, testplan_name, testplan_data)
+
+        self.assertFalse(success)
+
+    def test_insert_youtube_result_duplicate_video(self):
+        testplan_name = "test_plan"
+        testplan_data_1 = {
+            "args": {
+                "maxResults": 10,
+                "pageToken": "token",
+                "q": "test",
+                "regionCode": "US",
+                "relevanceLanguage": "en",
+                "topicId": "1",
+                "videoCategoryId": "1",
+                "videoDuration": "1",
+                "videoLicense": "1",
+            },
+            "items": [
+                {
+                    "title": "XD",
+                    "channelId": "1",
+                    "channelTitle": "1",
+                    "defaultAudioLanguage": "en",
+                    "videoId": "123",
+                    "results": {
+                        "modelName": "TestModel",
+                        "language": "en",
+                        "wer": 0.1,
+                        "mer": 1,
+                        "wil": 1,
+                        "wip": 1,
+                        "hits": 1,
+                        "substitutions": 12,
+                        "deletions": 12,
+                        "insertions": 65,
+                    },
+                    "duration": "12",
+                    "publishTime": "2022-01-01T00:00:00Z",
+                }
+            ],
+            "nextPageToken": "token",
+            "etag": "abc123",
+        }
+
+        testplan_data_2 = {
+            "args": {
+                "maxResults": 10,
+                "pageToken": "token",
+                "q": "test",
+                "regionCode": "US",
+                "relevanceLanguage": "en",
+                "topicId": "1",
+                "videoCategoryId": "1",
+                "videoDuration": "1",
+                "videoLicense": "1",
+            },
+            "items": [
+                {
+                    "title": "XD",
+                    "channelId": "1",
+                    "channelTitle": "1",
+                    "defaultAudioLanguage": "en",
+                    "videoId": "123",
+                    "results": {
+                        "modelName": "TestModel",
+                        "language": "en",
+                        "wer": 0.1,
+                        "mer": 1,
+                        "wil": 1,
+                        "wip": 1,
+                        "hits": 1,
+                        "substitutions": 12,
+                        "deletions": 12,
+                        "insertions": 65,
+                    },
+                    "duration": "12",
+                    "publishTime": "2022-01-01T00:00:00Z",
+                }
+            ],
+            "nextPageToken": "token",
+            "etag": "abc123",
+        }
+
+        insert_youtube_result(self.session, testplan_name, testplan_data_1)
+        success = insert_youtube_result(self.session, testplan_name, testplan_data_2)
+
+        self.assertFalse(success)
+
+
+if __name__ == "__main__":
+    unittest.main()
