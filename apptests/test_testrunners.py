5227ab278ad0f1f7e67112009d8c083248b26585
Gochad <malgorzata.drag@spoton.com>
2024-01-06 18:46:54 +0100

    fix mock open

--- apptests/test_testrunners.py
+++ apptests/test_testrunners.py
@@ -1,9 +1,13 @@
+import json
+import os
 import unittest
 from copy import deepcopy
-from os import environ, listdir
-from unittest.mock import Mock, patch, mock_open
+from os import environ, listdir, remove
+from unittest.mock import MagicMock, Mock, patch, mock_open
+from src.differs import jiwer_differ
 
 from generators.youtube_generator import generate
+import models
 from models.dummy_test import DummyTest
 from youtube_runner import YouTubeTestRunner
 
@@ -12,7 +16,7 @@ class Test_YoutubeTestRunner_run(unittest.TestCase):
     def setUp(self) -> None:
         self.tester = Mock()
         self.runner = YouTubeTestRunner(
-            DummyTest(), "Travel&Events_en_CAUQAQ_20231126-201522.json", "audio", iterations=1, 
+            "Travel&Events_en_CAUQAQ_20231126-201522.json", "audio", "./output", iterations=1, tester=self.tester
         )
         return super().setUp()
 
@@ -22,43 +26,53 @@ class Test_YoutubeTestRunner_run(unittest.TestCase):
         self.assertIsNotNone(self.runner.tester.differ)
         self.assertTrue("GoogleAPI" in environ)
 
-    @patch("src.transcript_test.TranscriptTest", spec=True)
+
+    @patch("models.dummy_test.DummyTest")
     @patch("generators.youtube_generator.generate")
     @patch("src.dataclasses.youtube_video.YouTubeVideo.from_dict")
-    @patch("builtins.open", create=True)
-    def test_run(self, mock_transcript_test, mock_generate, mock_from_dict, mock_open):
-        mock_transcriber = mock_transcript_test.transcriber
+    @patch("builtins.open", new_callable=mock_open)
+    @patch("json.load")
+    def test_run(self, mock_dummy_test, mock_generate, mock_from_dict, mock_open, mock_json_load):
+        mock_dummy_test_instance = Mock(spec=models.DummyTest)
+        mock_dummy_test_instance.transcriber = lambda x: "This is a model for tests :)"
+        mock_dummy_test_instance.normalizer = lambda x: x.lower()
+        mock_dummy_test_instance.differ = jiwer_differ
+        mock_dummy_test_instance.language = "en"
         runner = YouTubeTestRunner(
-            tester=mock_transcript_test,
+            tester=mock_dummy_test_instance,
             testplan_path="mock_testplan_path",
             audio_dir="mock_audio_dir",
+            output_dir="mock_output_dir",
             iterations=2,
             save_transcripts=True,
             save_to_database=True,
             keep_audio=False
         )
-
-        mock_open.return_value.read.return_value = '{"args": {"q": "test"}, "items": [{"videoId": "123"}], "nextPageToken": "token"}'
-
         mock_video = mock_from_dict.return_value
         mock_video.youtube_transcript.return_value = "Mock Target Transcript"
 
-        mock_video.download_mp3.return_value = "mock_audio"
+        mock_video.download_mp3.return_value = "./apptests/data/sample.mp3"
 
-        mock_transcriber.transcribe.return_value = "Mock Transcribe Result"
+        mock_dummy_test.return_value = mock_dummy_test_instance
+        mock_dummy_test_instance.compare.return_value = {"result": "Mock Compare Result"}
 
-        mock_transcript_test.compare.return_value = {"result": "Mock Compare Result"}
+        read_data = '{"args": {"q": "test"}, "items": [{"videoId": "123"}], "nextPageToken": "token"}'
 
-        runner.run()
+        mock_open_func = mock_open(read_data=read_data)
+        mock_open_func.return_value.__enter__.return_value.read.return_value = read_data
+
+        with patch("builtins.open", mock_open_func), patch("json.load") as mock_json_load:
+            mock_json_load.return_value = {"args": {"q": "test"}, "items": [{"videoId": "123"}], "nextPageToken": "token", "language": "en", "targetTranscript": "Mock Target Transcript"}
+
+            runner.run()
 
         mock_open.assert_called_once_with("mock_testplan_path", encoding="utf8")
         mock_generate.assert_called_once_with({"pageToken": "token"})
         mock_from_dict.assert_called_once_with({"videoId": "123"})
         mock_video.youtube_transcript.assert_called_once_with(mock_transcript_test.language)
         mock_video.download_mp3.assert_called_once_with("mock_audio_dir")
-        mock_transcriber.transcribe.assert_called_once_with("mock_audio")
-        mock_transcript_test.compare.assert_called_once_with("Mock Transcribe Result", "Mock Target Transcript")
-        mock_transcript_test.additional_info.assert_called_once()
+        mock_dummy_test.return_value.compare.assert_called_once_with("Mock Transcribe Result", "Mock Target Transcript")
+        mock_dummy_test.return_value.additional_info.assert_called_once()
 
     @patch("src.dataclasses.youtube_video.YouTubeVideo.from_dict")
     @patch("builtins.open")
@@ -151,15 +165,17 @@ testplan["items"] = [
 
 class Test_YoutubeTestRunner_save_results(unittest.TestCase):
     def setUp(self) -> None:
-        self.testrunner = YouTubeTestRunner(DummyTest(), "", "audio", iterations=1)
+        self.testrunner = YouTubeTestRunner("", "audio","./output", iterations=1, tester=Mock())
         return super().setUp()
 
+    # it didnt work - TypeError is raised when value is get from dict
     def test_none_dict(self):
-        self.assertRaises(KeyError, self.testrunner.save_results(None))
+        self.assertRaises(TypeError, self.testrunner.save_results(None))
 
         json_files = [file for file in listdir("apptests") if file.endswith(".json")]
         self.assertEqual(len(json_files), 2)
 
+    # it didnt work - KeyError is raised when value is get from dict
     def test_empty_dict(self):
         self.assertRaises(KeyError, self.testrunner.save_results({}))
         
@@ -167,9 +183,16 @@ class Test_YoutubeTestRunner_save_results(unittest.TestCase):
         self.assertEqual(len(json_files), 2)
 
     def test_valid_dict(self):
-        runner = YouTubeTestRunner(DummyTest(), "simpleTest.json", "audio", iterations=1)
+        path = "simpleTest.json"
+        runner = YouTubeTestRunner(path, "audio", "", iterations=1, tester=Mock())
         generated_testplan = generate(search_request.get("args"))
         runner.save_results(generated_testplan)
 
         json_files = [file for file in listdir() if file.endswith(".json")]
-        self.assertEqual(len(json_files), 3)
+        self.assertEqual(len(json_files), 1)
+
+        for json_file in json_files:
+            remove(json_file)
+
+
+        
